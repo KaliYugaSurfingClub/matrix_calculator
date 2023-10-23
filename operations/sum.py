7b0ba18afdf568a2f36f56c4d8fcdeb894ada0c8
ilya <ivchvanov@edu.hse.ru>
2023-10-23 15:31:35 +0300

    To snake_case

--- operations/sum.py
+++ operations/sum.py
@@ -1,2 +1,29 @@
-def get_sum_of_matrices(a, b):
-    return a, b
+def matrix_summing(first_matrix, second_matrix):
+    matrix_size1 = []
+    matrix_size1.append(len(first_matrix[1]))
+    matrix_size1.append(len(first_matrix[0]))
+    matrix_size2 = []
+    matrix_size2.append(len(second_matrix[1]))
+    matrix_size2.append(len(second_matrix[0]))
+    if matrix_size1 != matrix_size2:
+        return 'Матрицы разных размеров'
+    result = [[] for i in range(matrix_size1[0])]
+    for i in range(matrix_size1[0]):
+        for j in range(matrix_size1[1]):
+            result[i].append(first_matrix[i][j] + second_matrix[i][j])
+    return result
+
+def matrix_subtraction(first_matrix, second_matrix):
+    matrix_size1 = []
+    matrix_size1.append(len(first_matrix[1]))
+    matrix_size1.append(len(first_matrix[0]))
+    matrix_size2 = []
+    matrix_size2.append(len(second_matrix[1]))
+    matrix_size2.append(len(second_matrix[0]))
+    if matrix_size1 != matrix_size2:
+        return 'Матрицы разных размеров'
+    result = [[] for i in range(matrix_size1[0])]
+    for i in range(matrix_size1[0]):
+        for j in range(matrix_size1[1]):
+            result[i].append(first_matrix[i][j] - second_matrix[i][j])
+    return result
